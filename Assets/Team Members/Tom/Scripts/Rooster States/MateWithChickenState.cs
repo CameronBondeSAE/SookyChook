fa9f0f8378857802e6a66a056b6d1a70072d4dbe
Tom Bartle <tombartle96@gmail.com>
2021-10-27 15:32:01 +1100

    Added rooster planner and states

---
+++ Assets/Team Members/Tom/Scripts/Rooster States/MateWithChickenState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Anthill.AI;
+using UnityEngine;
+
+namespace Tom
+{
+    public class MateWithChickenState : AntAIState
+    {
+        public GameObject owner;
+    
+        public override void Create(GameObject aGameObject)
+        {
+            base.Create(aGameObject);
+
+            owner = aGameObject;
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Execute(float aDeltaTime, float aTimeScale)
+        {
+            base.Execute(aDeltaTime, aTimeScale);
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+    }
+}
