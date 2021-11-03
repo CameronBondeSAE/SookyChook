9dc14670a228000ab2cfd02b2b6953aacc4605d5
Tom Bartle <tombartle96@gmail.com>
2021-11-03 16:02:06 +1100

    Added steering behaviours

---
+++ Assets/Team Members/Tom/Scripts/Steering/Align.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tom
+{
+    public class Align : SteeringBase
+    {
+        public override Vector3 CalculateMove(List<Transform> neighbours)
+        {
+            if (neighbours.Count == 0)
+            {
+                return transform.forward;
+            }
+        
+            Vector3 alignmentMove = Vector3.zero;
+
+            foreach (Transform n in neighbours)
+            {
+                alignmentMove += n.transform.forward;
+            }
+
+            alignmentMove /= neighbours.Count;
+
+            return alignmentMove;
+        }
+
+        private void FixedUpdate()
+        {
+            rb.AddTorque(CalculateMove(GetNeighbours()));
+        }
+    }
+}
---
+++ Assets/Team Members/Tom/Scripts/Steering/SteeringBase.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tom
+{
+    public class SteeringBase : MonoBehaviour
+    {
+        protected Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public virtual Vector3 CalculateMove(List<Transform> neighbours)
+        {
+            return Vector3.zero;
+        }
+
+        public List<Transform> GetNeighbours()
+        {
+            Collider[] cols = Physics.OverlapSphere(transform.position, 5f);
+            List<Transform> neighbours = new List<Transform>();
+
+            foreach (Collider c in cols)
+            {
+                if (c.GetComponent<SteeringBase>())
+                {
+                    neighbours.Add(c.transform);
+                }
+            }
+
+            return neighbours;
+        }
+    }
+}
