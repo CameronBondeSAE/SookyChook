84d1c0192e5b33d7d2ec5082f60d5b21f3d83a66
Rob <1021549@student.sae.edu.au>
2021-11-03 14:40:33 +1100

    wildlife manager to be populated?

--- Assets/Team Members/Rob/Scripts/Wildlife Manager/WildLifeManager.cs
+++ Assets/Team Members/Rob/Scripts/Wildlife Manager/WildLifeManager.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Tom;
 
 namespace Rob
 {
-
     public class WildLifeManager : MonoBehaviour
     {
         [System.Serializable]
@@ -13,16 +14,48 @@ namespace Rob
             public GameObject[] wildlife;
             public int count;
             public float timeBetweenSpawns;
+            public bool maintainSpawnedNumber;
         }
-        
+
         public WildLife[] animalsToSpawn;
         public Transform[] spawnPoints;
         public List<GameObject> animalsSpawned;
 
 
+        private void Awake()
+        {
+        }
 
+        private void Start()
+        {
+            FindObjectOfType<DayNightManager>().PhaseChangeEvent += ChangePhase;
+        }
 
-
-
+        private void ChangePhase(DayNightManager.DayPhase timeOfDay)
+        {
+            // if (timeOfDay == DayNightManager.DayPhase.Morning)
+            // {
+            //     //insert spawn logic here?
+            //     Debug.Log(" its " + timeOfDay);
+            // }
+            //
+            // if (timeOfDay == DayNightManager.DayPhase.Noon)
+            // {
+            //     //insert spawn logic here?
+            //     Debug.Log(" its " + timeOfDay);
+            // }
+            //
+            // if (timeOfDay == DayNightManager.DayPhase.Evening)
+            // {
+            //     //insert spawn logic here?
+            //     Debug.Log(" its " + timeOfDay);
+            // }
+            //
+            // if (timeOfDay == DayNightManager.DayPhase.Night)
+            // {
+            //     //insert spawn logic here?
+            //     Debug.Log(" its " + timeOfDay);
+            // }
+        }
     }
 }
