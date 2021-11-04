796777822488500676dceec0465db9a0fa008b28
Rob <1021549@student.sae.edu.au>
2021-11-05 10:41:00 +1100

    wildlife manager will spawn according to current day phase

--- Assets/Scripts/Managers/WildLifeManager.cs
+++ Assets/Scripts/Managers/WildLifeManager.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Tanks;
 using UnityEngine;
 using Tom;
+using Random = UnityEngine.Random;
 
 namespace Rob
 {
@@ -11,28 +13,22 @@ namespace Rob
         [System.Serializable]
         public class WildLife
         {
-            public GameObject[] wildlife;
+            public GameObject[] animals;
             public int count;
-            public float timeBetweenSpawns;
-            public bool maintainSpawnedNumber;
+            public bool maintainNumberSpawned;
+            public int timeBetweenSpawns;
+            public DayNightManager.DayPhase phaseTime;
         }
 
-        public WildLife[] animalsToSpawn;
-        public Transform[] spawnPoints;
-        public List<GameObject> animalsSpawned;
 
-        
-        
-        //will delete this part eventually 
-        public GameObject animal;
-        public Transform spawnPoint;
-        // ^^
+        public WildLife[] wildLife;
 
-        
-        
         private WildLife currentWildLife;
-        private WildLife currentAnimal;
 
+        // add as many as you need for the animals needed to spawn
+        public Transform[] spawnPoints;
+
+        public List<GameObject> animalsSpawned;
 
         private void Awake()
         {
@@ -43,41 +39,24 @@ namespace Rob
             FindObjectOfType<DayNightManager>().PhaseChangeEvent += ChangePhase;
         }
 
-        
-        private void ChangePhase(DayNightManager.DayPhase timeOfDay)
+        public void ChangePhase(DayNightManager.DayPhase timeOfDay)
         {
-            if (timeOfDay == DayNightManager.DayPhase.Midnight)
+            for (int i = 0; i < wildLife.Length; i++)
             {
-                Instantiate(animal, spawnPoint.position, spawnPoint.rotation);
-                Debug.Log(" its " + timeOfDay);
-            }
-            
-            if (timeOfDay == DayNightManager.DayPhase.Morning)
-            {
-                //insert spawn logic here?
-                Instantiate(animal, spawnPoint.position, spawnPoint.rotation);
-                Debug.Log(" its " + timeOfDay);
-            }
+                if (wildLife[i].phaseTime == timeOfDay)
+                {
+                    currentWildLife = wildLife[i];
 
-            if (timeOfDay == DayNightManager.DayPhase.Noon)
-            {
-                //insert spawn logic here?
-                Instantiate(animal, spawnPoint.position, spawnPoint.rotation);
-                Debug.Log(" its " + timeOfDay);
-            }
-
-            if (timeOfDay == DayNightManager.DayPhase.Evening)
-            {
-                //insert spawn logic here?
-                Instantiate(animal, spawnPoint.position, spawnPoint.rotation);
-                Debug.Log(" its " + timeOfDay);
-            }
-
-            if (timeOfDay == DayNightManager.DayPhase.Night)
-            {
-                //insert spawn logic here?
-                Instantiate(animal, spawnPoint.position, spawnPoint.rotation);
-                Debug.Log(" its " + timeOfDay);
+                    for (int j = 0; j < wildLife[i].count; j++)
+                    {
+                        Transform randomTransform = spawnPoints[Random.Range(0, spawnPoints.Length)];
+                        GameObject randomWildlife =
+                            currentWildLife.animals[Random.Range(0, currentWildLife.animals.Length)];
+                        GameObject spawnedWildlife = Instantiate(randomWildlife, randomTransform.position,
+                            randomTransform.rotation);
+                        animalsSpawned.Add(spawnedWildlife);
+                    }
+                }
             }
         }
     }
