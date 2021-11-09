d81a94c3d9108f268428b836f346e3d7f8cc44b4
Rob <1021549@student.sae.edu.au>
2021-11-09 17:26:25 +1100

    wildlife manager, workingish

--- Assets/Scripts/Managers/WildLifeManager.cs
+++ Assets/Scripts/Managers/WildLifeManager.cs
@@ -25,6 +25,7 @@ namespace Rob
 
         public WildLife[] wildLife;
         public bool clearList;
+        public float groundOffset;
         private WildLife currentWildLife;
 
         // add as many as you need for the animals needed to spawn
@@ -53,15 +54,24 @@ namespace Rob
                     {
                         Transform randomTransform = spawnPoints[Random.Range(0, spawnPoints.Length)];
                         Vector3 spawnPos = randomTransform.position;
-                        spawnPos = new Vector3(spawnPos.x, spawnPos.y+ 5, spawnPos.z);
+                        spawnPos = new Vector3(spawnPos.x, spawnPos.y + 5, spawnPos.z);
                         Vector3 randomSpot = Random.insideUnitCircle * 5;
                         randomSpot.z = randomSpot.y; //hack, im sure there is an easier way to do this
-                        randomSpot.y = spawnPos.y; //hack, im sure there is an easier way to do this
                         Debug.Log(randomSpot);
                         GameObject randomWildlife =
                             currentWildLife.animals[Random.Range(0, currentWildLife.animals.Length)];
                         GameObject spawnedWildlife = Instantiate(randomWildlife, spawnPos + randomSpot,
                             randomTransform.rotation);
+                        RaycastHit hit;
+                        
+                        if (Physics.Raycast(spawnedWildlife.transform.position, Vector3.down, out hit, 20))
+                        {
+                            Debug.DrawRay(spawnedWildlife.transform.position, Vector3.down * hit.distance, Color.blue);
+                            Debug.Log(hit.distance);
+                            spawnedWildlife.transform.position = new Vector3(spawnedWildlife.transform.position.x,
+                                spawnedWildlife.transform.position.y - (hit.distance - groundOffset) ,
+                                spawnedWildlife.transform.position.z);
+                        }
                         animalsSpawned.Add(spawnedWildlife);
                     }
                 }
