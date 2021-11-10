1656a2342b503775301e5bcb13ef4f5a60778885
Tom Bartle <tombartle96@gmail.com>
2021-11-10 12:38:58 +1100

    Added functions to health script

--- Assets/Scripts/Reusable components/Health.cs
+++ Assets/Scripts/Reusable components/Health.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Allows each object to have unique health variable
+    [SerializeField]
+    float currentHealth;
+    [SerializeField]
+    float maxHealth;
+    [SerializeField]
+    float deathThreshold;
+
+    //DEATH EVENT
+    public delegate void DeathSignature();
+    public event DeathSignature DeathEvent;
+    
+    private void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Manage Object Health (Can both increase or decrease health)
+    public void ChangeHealth(float amount)
     {
-        
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        //If health ever drops to 0 or below fire off DeathEvent
+        if(currentHealth <= deathThreshold)
+        {
+            DeathEvent?.Invoke();
+        }
+    }
+
+    //GET HEALTH
+    public float GetHealth()
+    {
+        return currentHealth;
     }
 }
