59e05a7fe1d6db38c4056dc67e7b02d3a8999f9d
Rob <1021549@student.sae.edu.au>
2021-11-17 11:35:49 +1100

    chicken zombies, spawns players

--- Assets/Scripts/Managers/Game Modes/ChickenZombiesMode.cs
+++ Assets/Scripts/Managers/Game Modes/ChickenZombiesMode.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class ChickenZombiesMode : GameModeBase
 {
-
     public GraveyardManager graveyardManager;
-    
-    
+    public CharacterModel[] players;
+    public Transform[] playerSpawns;
+
+
     public override void Activate()
     {
         base.Activate();
-        
+
         Debug.Log("Chicken Zombs Activate");
+        for (int i = 0; i < players.Length; i++)
+        {
+            for (int x = 0; x < playerSpawns.Length; x++)
+            {
+                if (playerSpawns != null)
+                {
+                    Instantiate(players[i], playerSpawns[x].position, playerSpawns[x].rotation);
+                }
+            }
+        }
     }
 }
