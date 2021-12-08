a3f3332d1c8334ae002174c2ea7849119a51153e
Tom Bartle <tombartle96@gmail.com>
2021-12-08 12:19:37 +1100

    Added order types to gamemode

--- Assets/Scripts/Managers/Game Modes/ChickenGrowingMode.cs
+++ Assets/Scripts/Managers/Game Modes/ChickenGrowingMode.cs
@@ -1,12 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Aaron;
+using Tom;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ChickenGrowingMode : GameModeBase
 {
     public List<CharacterModel> players;
     public Transform[] playerSpawns = new Transform[4];
-    
+
+    //public OrderDropoffZone orderZone;
+
+    [Serializable]
+    public class Order
+    {
+        public ProductType productType;
+        public int amount;
+    }
+
+    public enum ProductType
+    {
+        Chicken,
+        Egg,
+        Rooster
+    }
+
+    public List<Order> possibleOrders;
+
+    public event Action<Order> NewOrderEvent;
+
     public override void Activate()
     {
         base.Activate();
@@ -18,5 +42,33 @@ public class ChickenGrowingMode : GameModeBase
                 Instantiate(players[i], playerSpawns[i].position, playerSpawns[i].rotation);
             }
         }
+        
+        NewOrder();
+
+        // DayNightManager.Instance.PhaseChangeEvent += OrderCheck;
+        // ChickenManagerEvent += ChickenCheck;
+    }
+    
+    
+    public void ChickenCheck()
+    {
+        if (ChickenManager.Instance.chickensList.Count <= 0)
+        {
+            EndMode();
+        }
+    }
+
+    // public void OrderCheck(DayNightManager.DayPhase phase)
+    // {
+    //     if (phase == DayNightManager.DayPhase.Evening && !orderZone.orderCompleted)
+    //     {
+    //         EndMode();
+    //     }
+    // }
+
+    public void NewOrder()
+    {
+        Order newOrder = possibleOrders[Random.Range(0, possibleOrders.Count)];
+        NewOrderEvent?.Invoke(newOrder);
     }
 }
