858998dcf781cbd5b1086a77d4e791565fce9aa1
JohnEngstrom <engstromj124@gmail.com>
2021-12-15 16:02:27 +1100

    Added Cash Manager + Seed Planter UI & Seed shop refills

--- Assets/Team Members/John/Scripts/Shop Stuff/SeedPlanter_ShopPanel.cs
+++ Assets/Team Members/John/Scripts/Shop Stuff/SeedPlanter_ShopPanel.cs
@@ -9,15 +9,16 @@ public class SeedPlanter_ShopPanel : MonoBehaviour
 {
     [Header("Text Field References")]
     [SerializeField]
-    TMP_Text price;
+    TMP_Text upgradePriceText;
+    [SerializeField]
+    TMP_Text refillPriceText;
     [SerializeField]
     TMP_Text description;
 
     int upgradePrice;
+    public int seedRefillPrice = 250;
 
-    //SeedPlanterModel[] seedPlanters;
     public SeedPlanterModel seedPlanter;
-    public ShopManager shopManager;
 
     //Event
     public event Action PlanterUpgradedEvent;
@@ -37,6 +38,15 @@ public class SeedPlanter_ShopPanel : MonoBehaviour
 
     void UpdatePrice()
     {
+        if(seedPlanter.seedsAvailable == seedPlanter.maxSeeds)
+        {
+            refillPriceText.text = "Full";
+        }
+        else if(seedPlanter.seedsAvailable < seedPlanter.maxSeeds)
+        {
+            refillPriceText.text = "BUY: " + seedRefillPrice;
+        }
+
         if(seedPlanter.planterLevel == 1)
         {
             //Level 2 Upgrade Price
@@ -50,7 +60,7 @@ public class SeedPlanter_ShopPanel : MonoBehaviour
         }
         else
         {
-            price.text = "Max Level";
+            upgradePriceText.text = "Max Level";
             description.text = "Seed Planter Fully Upgraded";
             return;
         }
@@ -61,7 +71,7 @@ public class SeedPlanter_ShopPanel : MonoBehaviour
 
     void UpdatePriceText()
     {
-        price.text = "BUY: " + upgradePrice;
+        upgradePriceText.text = "BUY: " + upgradePrice;
     }
 
     public void BuyUpgrade()
@@ -72,19 +82,28 @@ public class SeedPlanter_ShopPanel : MonoBehaviour
             return;
         }
 
-        if (shopManager.totalMoney >= upgradePrice && seedPlanter.planterLevel != seedPlanter.maxlevel)
+        if (CashManager.Instance.totalMoney >= upgradePrice && seedPlanter.planterLevel != seedPlanter.maxlevel)
         {
             PlanterUpgradedEvent?.Invoke();
-            shopManager.totalMoney -= upgradePrice;
+            CashManager.Instance.TakeMoney(upgradePrice);
             return;
         }
 
-        if(shopManager.totalMoney < upgradePrice)
+        if(CashManager.Instance.totalMoney < upgradePrice)
         {
             Debug.Log("Too Expensive");
 
             //Trigger EFX/SFX if item is too expensive to purcase (Player feedback)
             TooExpensiveEvent?.Invoke();
+        }
+    }
+
+    public void BuySeedRefill()
+    {
+        if(seedPlanter.seedsAvailable < seedPlanter.maxSeeds && CashManager.Instance.totalMoney > seedRefillPrice)
+        {
+            seedPlanter.seedsAvailable = seedPlanter.maxSeeds;
+            CashManager.Instance.TakeMoney(seedRefillPrice);
         }
     }
 }
--- Assets/Team Members/John/Scripts/Shop Stuff/ShopManager.cs
+++ Assets/Team Members/John/Scripts/Shop Stuff/ShopManager.cs
@@ -3,31 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class ShopManager : MonoBehaviour
+public class ShopManager : ManagerBase<ShopManager>
 {
-    public int totalMoney = 5000;
-    public TMP_Text moneyUI;
-
-    public AudioSource audioSource;
-    public AudioClip buySound;
-
-    public SeedPlanter_ShopPanel seedPlanterShop;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        seedPlanterShop.PlanterUpgradedEvent += OnShopBuy;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        moneyUI.text = "$" + totalMoney;
-    }
-
-    void OnShopBuy()
-    {
-        audioSource.clip = buySound;
-        audioSource.Play();
-    }
+    
 }
