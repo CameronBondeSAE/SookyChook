11a758189f84f0bbe77fbc3ba58754eeea0946a1
Tom Bartle <tombartle96@gmail.com>
2022-03-01 11:12:10 +1100

    Added networking to seeds

--- Assets/Scripts/Unique to one object/GrassEdible.cs
+++ Assets/Scripts/Unique to one object/GrassEdible.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.Netcode;
 using UnityEngine;
 
 public class GrassEdible : Edible
 {
 	public ParticleSystem particleSystem;
 	
+	public AudioSource audioSource;
+	public AudioClip grassClip;
+
+	private void Start()
+	{
+		// Network spawned from server, don't need ClientRpc
+		audioSource.clip = grassClip;
+		audioSource.Play();
+		transform.DOShakeRotation(0.5f);
+	}
+
 	public override void BeingEaten(float amount)
 	{
 		base.BeingEaten(amount);
--- Assets/Team Members/Lachlan/Scripts/Seeds.cs
+++ Assets/Team Members/Lachlan/Scripts/Seeds.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using Sirenix.OdinInspector;
+using Unity.Netcode;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
 public class Seeds : MonoBehaviour, IWaterable
 {
-    [Header("Seed Properties")]
-    public GameObject target;
+    [Header("Seed Properties")] public GameObject target;
     public GameObject grassSpawn;
 
     public bool Crying = false;
@@ -19,31 +19,38 @@ public class Seeds : MonoBehaviour, IWaterable
     public AudioClip grassClip;
 
     // Start is called before the first frame update
-    
+
     void Growth(bool isCrying)
     {
         if (isCrying)
         {
             Crying = true;
-            
         }
         else
         {
             Crying = false;
         }
     }
-    
-    
+
+
     //Interfaces
+    [Button]
     public void BeingWatered(float amount)
     {
-        //Audio Source is Quiet
-        audioSource.clip = grassClip;
-        audioSource.Play();
-            
-        //Destroy Seeds and Grow Grass
-        GameObject.Destroy(gameObject);
-        GameObject newGrass = GameObject.Instantiate(grassSpawn, transform.localPosition, Quaternion.identity);
+        if (NetworkManager.Singleton.IsServer)
+        {
+            //Destroy Seeds and Grow Grass
+            GameObject.Destroy(gameObject);
+            GameObject newGrass = GameObject.Instantiate(grassSpawn, transform.localPosition, Quaternion.identity);
+            newGrass.GetComponent<NetworkObject>().Spawn();
+
+            //PlayGrassEffectsClientRpc(newGrass);
+        }
+    }
+
+    [ClientRpc]
+    void PlayGrassEffectsClientRpc(GameObject newGrass)
+    {
         newGrass.transform.DOShakeRotation(0.5f);
     }
 }
